7ecadcbe7fe0f617c4025b06980a4e35098a2981
Betsa-lopez <lopezbetsabe306@gmail.com>
2023-11-17 21:36:27 -0600

    Se añadio la base de iniciar sesion

---
+++ src/app/iniciar-sesion/page.tsx
@@ -0,0 +1,15 @@
+import styles from '../page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Iniciar sesion</h1>
+      <br />
+      <input type="text"placeholder='Usuario' />
+      <br />
+      <input type="password" name="Pass"placeholder='Contraseña' id="" />
+      <br />
+      <button>Ingresar</button>
+    </main>
+  )
+}
---
+++ src/app/productos/page.tsx
@@ -0,0 +1,9 @@
+import styles from '../page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Pantalla de producto</h1>
+    </main>
+  )
+}
