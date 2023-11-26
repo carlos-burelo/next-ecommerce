e03d91e45d35f79d0267b468f96cfb7604f42a33
carlos-burelo <carlosfernandoburelo@gmail.com>
2023-11-26 16:40:10 -0600

    se modificaron los estilos por defecto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,11 @@
 import styles from "./page.module.css";
-import Image from "next/image";
-import Link from "next/link";
+
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.principal}>
-        <div className={styles.fondo}>
-          <div className={styles.cajaimagen}>
-            <img src="/INICIO.jpg" alt="" className={styles.imagen} />
-          </div>
-          <div className={styles.titulo1}>
-            <h1 className={styles.titulo}>PAPELAPP</h1>
-          </div>
-        </div>
-      </div>
-      <div className={styles.prueba2}>
-        <h3 className={styles.prueba}>sdn</h3>
-      </div>
-      <div className={styles.contenedor}>
-        <h3 className={styles.subtitulo}>Ven y compra los mejores productos</h3>
-        <Link className={styles.link} href="#">
-          <button className={styles.registro}>Registrate</button>
-        </Link>
-        <Link className={styles.link} href="/iniciar-sesion">
-          <button className={styles.Sesion}>Iniciar Sesion</button>
-        </Link>
+      <img src="./INICIO.jpg" alt="fondo de papeleria" className="bg" />
+      <div className="content">
+
       </div>
     </main>
   );
