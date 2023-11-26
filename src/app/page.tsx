831ce8860c9b57f5ca81e4da1449949bcb70156f
carlos-burelo <carlosfernandoburelo@gmail.com>
2023-11-26 16:48:04 -0600

    se cambio la home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <img src="./INICIO.jpg" alt="fondo de papeleria" className="bg" />
-      <div className="content">
-
-      </div>
     </main>
   );
 }
