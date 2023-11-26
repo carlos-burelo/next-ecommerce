6077bb1c01299a657d3be0ae600a22d290905c77
ManuelFulls <dediosdiego606@gmail.com>
2023-11-26 15:54:47 -0600

    cambios

--- src/app/iniciar-sesion/page.tsx
+++ src/app/iniciar-sesion/page.tsx
@@ -1,15 +1,32 @@
-import styles from "../page.module.css";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Iniciar sesion</h1>
-      <br />
-      <input type="text" placeholder="Usuario" />
-      <br />
-      <input type="password" name="Pass" placeholder="Contraseña" id="" />
-      <br />
-      <button>Ingresar</button>
+      <div className={styles.contenedor}>
+        <h1 className={styles.titulo}>Iniciar sesion</h1>
+
+        <div className={styles.formulario}>
+          <p className={styles.corre}>Correo</p>
+          <div className={styles.cont}>
+            <input type="text" placeholder="Correo" className={styles.correo} />
+          </div>
+
+          <p className={styles.contra}>Contraseña</p>
+          <div className={styles.conta}>
+            <input
+              type="password"
+              name="Pass"
+              placeholder="Contraseña"
+              id=""
+              className={styles.contraseña}
+            />
+          </div>
+
+          <button className={styles.boton}>Ingresar</button>
+        </div>
+      </div>
     </main>
   );
 }
