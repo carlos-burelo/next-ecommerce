391e00bbe9d0d9219fb5d4162008f6cacd7bb2ba
ManuelFulls <dediosdiego606@gmail.com>
2023-11-26 18:59:44 -0600

    numa

--- src/app/iniciar-sesion/page.tsx
+++ src/app/iniciar-sesion/page.tsx
@@ -1,31 +1,22 @@
-import styles from "../page.module.css";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <img src="/INICIO.jpg" alt="" className="bg" />
       <div className={styles.contenedor}>
-        <h1 className={styles.titulo}>Iniciar sesion</h1>
-        <div className={styles.formulario}>
-          <p className={styles.corre}>Correo</p>
-          <div className={styles.cont}>
-            <input type="text" placeholder="Correo" className={styles.correo} />
-          </div>
-
-          <p className={styles.contra}>Contraseña</p>
-          <div className={styles.conta}>
-            <input
-              type="password"
-              name="Pass"
-              placeholder="Contraseña"
-              id=""
-              className={styles.contraseña}
-            />
-          </div>
-          <div className={styles.bto}>
-            <button className={styles.boton}>Ingresar</button>
-          </div>
-        </div>
+        <h1 className={styles.tit1}>Iniciar sesion</h1>
+        <p>Correo</p>
+        <input type="text" placeholder="Correo" className={styles.correo} />
+        <p>Contraseña</p>
+        <input
+          type="password"
+          name="Pass"
+          placeholder="Contraseña"
+          id=""
+          className={styles.contraseña}
+        />
+        <button>Ingresar</button>
       </div>
     </main>
   );
