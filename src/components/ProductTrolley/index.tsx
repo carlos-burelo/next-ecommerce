f485ed3bfede9ee10fe9ee96cc95ffbe13b6c6e2
ManuelFulls <dediosdiego606@gmail.com>
2023-11-26 21:39:09 -0600

    carrito a medias

--- src/components/ProductTrolley/index.tsx
+++ src/components/ProductTrolley/index.tsx
@@ -2,9 +2,17 @@ import styles from "./ProductTrolley.module.css";
 
 export default function ProductTrolley() {
   return (
-    <div className={styles.ProductTrolley}>
-      <div className={styles.ProductImage}></div>
-      <div className={styles.ProductInfo}></div>
+    <div className={styles.productTrolley}>
+      <div className={styles.s}>
+        <img src="/Jansport.png" alt="" className={styles.imagen} />
+      </div>
+      <div className={styles.CajaInfo}>
+        <h3>Mochila</h3>
+        <p>
+          Marca Jansport estilo urbano
+          <span>$250</span>
+        </p>
+      </div>
     </div>
   );
 }
