852fe73941594caa3e74e0dc89e63b2848347caf
ManuelFulls <dediosdiego606@gmail.com>
2023-11-26 19:43:03 -0600

    pantalla de carrito a medias

---
+++ src/app/carrito/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+import ProductTrolley from "@/components/ProductTrolley";
+
+export default function home() {
+  return (
+    <main>
+      <Header></Header>
+      <ProductTrolley />
+      <div></div>
+    </main>
+  );
+}
---
+++ src/components/ProductTrolley/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./ProductTrolley.module.css";
+
+export default function ProductTrolley() {
+  return (
+    <div className={styles.ProductTrolley}>
+      <div className={styles.ProductImage}></div>
+      <div className={styles.ProductInfo}></div>
+    </div>
+  );
+}
