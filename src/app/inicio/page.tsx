64094060655b30a3778bc6e34231ec221de19214
Betsa-lopez <lopezbetsabe306@gmail.com>
2023-11-27 17:42:19 -0600

    Se agrego botones

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -3,6 +3,9 @@ export default function Home() {
   return (
     <main>
       <img src="./Inicio.jpg" className="bg" />;
+      <h1 className={styles.tit2}>Ven y compra los mejores productos</h1>
+      <button className={styles.boton}>Crear cuenta</button>
+      <button className={styles.boton}>Iniciar sesion</button>
     </main>
   );
 }
