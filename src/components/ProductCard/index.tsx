8e317ada724909237d656d4a6b6a81fc6f27331d
carlos-burelo <carlosfernandoburelo@gmail.com>
2023-11-27 22:23:18 -0600

    se corrigieron estilos

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,15 +1,12 @@
+import Image from 'next/image'
 import styles from './ProductCard.module.css'
 
 export default function ProductCard(producto: Producto) {
   return (
     <div className={styles.productCard}>
-      <div className={styles.productImageContainer}>🖼️</div>
-      <div className={styles.productInfo}>
-        <h3 className={styles.productName}>{producto.nombre}</h3>
-        <p className={styles.productShop}>
-          <span className={styles.productPrice}>{producto.costo} MXN</span>
-        </p>
-      </div>
+      <span className={styles.productPrice}>{producto.costo} MXN</span>
+      <img src='/Jansport.png' alt='IMAGE' width={100} height={100} />
+      <h3 className={styles.productName}>{producto.nombre}</h3>
     </div>
   )
 }
